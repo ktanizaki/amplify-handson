66a5d7292c10f715b5236ff54a053e92ab060276
ktanizaki <ec2-user@ip-172-31-72-199.ec2.internal>
2020-05-30 01:51:28 +0000

    Add Auth function

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,58 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <li><router-link to="/ai">AIアプリ</router-link></li>
-      <li><router-link to="/chat">チャットアプリ</router-link></li>
-      <li><router-link to="/analytics">分析アプリ</router-link></li>
-      <li><router-link to="/webpush">Web Pushアプリ</router-link></li>
+    <div v-if="signedIn">
+      <div id="nav">
+        <div class="amplify-sign-out">
+          <amplify-sign-out/>
+          ようこそ、 {{username}} さん
+        </div>
+        <li><router-link to="/ai">AIアプリ</router-link></li>
+        <li><router-link to="/chat">チャットアプリ</router-link></li>
+        <li><router-link to="/analytics">分析アプリ</router-link></li>
+        <li><router-link to="/webpush">Web Pushアプリ</router-link></li>
+      </div>
       <router-view />
+    </div>
+    <div v-else>
+      <amplify-authenticator />
     </div>
   </div>
 </template>
+
+<script>
+import { Auth } from 'aws-amplify'
+import { AmplifyEventBus } from 'aws-amplify-vue'
+
+window.LOG_LEVEL = 'VERBOSE';
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      signedIn: false,
+      username: ''
+    }
+  },
+  async beforeCreate() {
+    try {
+      let cognitoUser = await Auth.currentAuthenticatedUser()
+      this.signedIn = true
+      this.username = cognitoUser.username
+    } catch (err) {
+      this.signedIn = false
+    }
+    
+    AmplifyEventBus.$on('authState', async info => {
+      if (info === 'signedIn') {
+        let cognitoUser = await Auth.currentAuthenticatedUser()
+        this.signedIn = true
+        this.username = cognitoUser.username
+      } else {
+        this.signedIn = false
+      }
+    });
+  }
+};
+</script>
 
 <style src="./assets/style.css" />
